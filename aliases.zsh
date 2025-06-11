282a5c73b6bb0f42ff9fcab5bbf72f6f5dac8069
nilsonsmf <51447494+nilsonsmf@users.noreply.github.com>
2025-06-11 20:29:11 +0100

    Create aliases.zsh

---
+++ aliases.zsh
@@ -0,0 +1,14 @@
+alias ls="exa --icons"
+alias la="exa --icons -la"
+alias bat="batcat --style=auto"
+alias vim="vim"
+alias ll="exa --icons -l"
+alias ports='netstat -tulanp'
+alias meminfo='free -m -l -t'
+alias now='date +"%T"'
+alias h='history'
+alias vi='vim'
+alias subl='"/mnt/c/Program Files/Sublime Text 3/subl.exe"'
+alias i='sudo apt install'
+alias r='sudo apt remove'
+alias s='sudo apt search'
